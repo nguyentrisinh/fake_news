ad2620466d17017baf4bae8e9238eea31e53e622
Unknown <liemnt.uit@gmail.com>
2018-04-06 01:03:36 +0700

    change crawl thanhnien

--- news_crawler/news_crawler/spiders/thanhnien_spider.py
+++ news_crawler/news_crawler/spiders/thanhnien_spider.py
@@ -9,6 +9,7 @@ from dateutil import parser
 # from .items import NewsItem
 import scrapy
 from ..items import NewsDetailItem
+from crawler_engine.models import NewsDetail
 
 MAX_PAGE = 10
 
@@ -21,19 +22,32 @@ class ThanhNienSpider(CrawlSpider):
     start_urls = [
         "https://thanhnien.vn"
     ]
+    crawledLinks = []
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//nav[@class='site-header__nav']//a"), follow=True),
+        Rule(LinkExtractor(allow='(%s)\/([\/\w-])*[0-9]*\.html$' % allowed_categories_re), callback="parse_item"),
+        # Rule(LinkExtractor(restrict_xpaths="//nav[@class='site-header__nav']//a"), follow=True),
 
-        Rule(LinkExtractor(allow='(%s)\/((trang-[0-5].html$)|(^$))' % allowed_categories_re), callback="parse_page",
-             follow=True),
+        Rule(LinkExtractor(allow='(%s)((\/page\/[0-5].html$)|(/$)|$)' % allowed_categories_re), follow=True),
     )
 
+    def check_already_crawled_url(self, response):
+        url = response.url
+        url_crawled_list = NewsDetail.objects.all().values_list('url', flat=True)
+
+        if url in url_crawled_list:
+            return False
+
+        return True
+
     def parse_item(self, response):
+        if not self.check_already_crawled_url(response):
+            return None
+        if response.url in self.crawledLinks:
+            return None
+        self.crawledLinks.append(response.url)
         hxs = Selector(response)
         item = NewsDetailItem()
-
-        # base_url, url, title, top_image_url, details, authors, category, keywords, published_date, status, crawled,
-        # created_at, updated_at
+        # base_url, url, title, top_image_url, details, authors, category, keywords, published_date, status, crawled, created_at, updated_at
         item['base_url'] = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(response.url))
 
         item['url'] = response.url
@@ -44,19 +58,21 @@ class ThanhNienSpider(CrawlSpider):
         # if (hxs.select('//img[@class="storyavatar"]/@src')):
         #     top_img_url = hxs.select('//img[@class="storyavatar"]/@src').extract()
         # else:
-        if hxs.select('//div[@class="pswp-content__image"]//img'):
+        if (hxs.select('//div[@class="pswp-content__image"]//img')):
             top_img_url = hxs.select('//div[@class="pswp-content__image"]//img/@src')[0].extract()
         item['top_image_url'] = top_img_url
 
-        item['details'] = ''.join(hxs.select('//div[@id="main_detail"]//div/text()').extract()).strip()+'\n'
+        item['details'] = ''.join(hxs.select('//div[@id="main_detail"]//div/text()').extract()).strip() + '\n'
 
-        item['authors'] = ''.join(hxs.select('//div[@class="details__author__meta" and position()=1]//text()').extract()).strip()+'\n'
+        item['authors'] = ''.join(
+            hxs.select('//div[@class="details__author__meta" and position()=1]//text()').extract()).strip() + '\n'
 
-        item['category']= ''.join(hxs.select('//meta[@property="article:section"]/@content').extract()).strip()+'\n'
+        item['category'] = ''.join(hxs.select('//meta[@property="article:section"]/@content').extract()).strip() + '\n'
 
         item['keywords'] = ''.join(hxs.select('//meta[@name="keywords"]/@content').extract()).strip() + '\n'
 
-        item['published_date'] = parser.parse(''.join(hxs.select('//meta[@itemprop="datePublished"]/@content').extract()).strip() + '\n')
+        item['published_date'] = parser.parse(
+            ''.join(hxs.select('//meta[@itemprop="datePublished"]/@content').extract()).strip() + '\n')
 
         item['status'] = 1
 
@@ -68,11 +84,12 @@ class ThanhNienSpider(CrawlSpider):
 
         return item
 
-    def parse_page(self, response):
-        links = response.xpath('//article[@class="story"]/a/@href').extract()
-        crawledLinks = []
-        for link in links:
-            if link not in crawledLinks:
-                link = 'https://thanhnien.vn%s' % link
-                crawledLinks.append(link)
-                yield Request(link, self.parse_item)
+        # def parse_page(self, response):
+        #     links = response.xpath('//article[@class="story"]/a/@href').extract()
+        #
+        #     crawledLinks = []
+        #     for link in links:
+        #         if link not in crawledLinks:
+        #             link = 'https://thanhnien.vn%s' % link
+        #             crawledLinks.append(link)
+        #             yield Request(link, self.parse_item)
