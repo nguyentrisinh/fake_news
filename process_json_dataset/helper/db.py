160c80a31d5deac38ae9efd73fa3d2fd3b41bba7
Unknown <nguyentrisinh0810@gmail.com>
2018-06-26 21:45:55 +0700

    Change into main function

--- process_json_dataset/helper/db.py
+++ process_json_dataset/helper/db.py
@@ -3,18 +3,21 @@ import psycopg2
 class Connection():
     hostname = 'localhost'
     username = 'postgres'  # the username when you create the database
-    password = '123456'  # change to your password
-    # password = 'sinh1996'  # change to your password
-    database = 'fake_news_db'
-    # database = 'fake_news_db_test'
+    # password = '123456'  # change to your password
+    password = 'sinh1996'  # change to your password
+    # database = 'fake_news_db'
+    database = 'fake_news_db_test'
     conn = None
+
     def connect(self):
         self.conn = psycopg2.connect(host=self.hostname, user=self.username, password=self.password,
                                      dbname=self.database)
+
     def query(self,query):
         cur = self.conn.cursor()
         cur.execute(query)
         return cur.fetchall()
+
     def insert(self,query):
         cur = self.conn.cursor()
         cur.execute(query)
